c98939fbbc2d5fd489ef72fc6d93e55d72cf7ed7
Debarati Jash <87527679+roshnijash@users.noreply.github.com>
2022-09-30 22:10:51 +0530

    Add files via upload

---
+++ Task-2/calculator.js
@@ -0,0 +1,49 @@
+
+function dis(val)
+{
+    document.getElementById("result").value+=val
+}
+
+function solve()
+{
+    let x=document.getElementById("result").value
+    let y=eval(x)
+    document.getElementById("result").value=y
+}
+
+function clr()
+{
+    document.getElementById("result").value=" "
+}
+function showTime()
+{
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0)
+      {
+       h = 12;
+      }
+    
+    if(h > 12)
+    {
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("DebaratiClock").innerText = time;
+    document.getElementById("DebaratiClock").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
